6f287816ec7bd0a4a1ad5e946f62711a57fc03ca
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-03-09 11:54:55 +0545

    Fix import error in tests.py by using absolute import

--- django_CRUD/solutions_api/tests.py
+++ django_CRUD/solutions_api/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
-from .models import Note
+from solutions_api.models import Note
 
 class NoteTests(APITestCase):
     def test_notes_can_be_created(self):

21befad2190890068aedff621d2a927dbefdecfd
Pranjal Poudel <pranjalpaudel62@gmail.com>
2026-03-09 10:50:54 +0545

    Add fallbacks to simplify deployment

--- django_CRUD/django_CRUD/settings.py
+++ django_CRUD/django_CRUD/settings.py
@@ -25,12 +25,12 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-fallback-for-lab-only-12345")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
